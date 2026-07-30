4664bc1896f7c626740748b4bc8e9e01a23c721b
Meradane <shardy689@gmail.com>
2026-07-30 08:50:27 +0100

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,10 +1,5 @@
-// Add a new Skill
 Hooks.once("init", () => {
-  CONFIG.DND5E.skills.backflip = {
-    label: "Backflip",
-    ability: "dex",
-    fullKey: "backflip", // Full key used in enrichers
-    reference: "Compendium.my-module…", // UUID of journal entry page for rich tooltips
-    icon: "…" // Icon used in favorites on new character sheet
-  };
+  CONFIG.DND5E.languages.coolspeech = "Cool Speech";
+  CONFIG.DND5E.languages.standard.children.supercommon = "Super Common";
+  CONFIG.DND5E.languages.exotic.children.uncommon = "Uncommon";
 });
